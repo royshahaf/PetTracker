defa7d7b521224d66b9416308e3846a20bda2706
Peter <Peter@LAPTOP-0RJVOTU0>
2018-10-24 21:08:49 +0300

    get rid of unneeded equals / hashCode methods

--- src/main/java/entities/Track.java
+++ src/main/java/entities/Track.java
@@ -22,40 +22,6 @@ public class Track implements Serializable {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((fused == null) ? 0 : fused.hashCode());
-		result = prime * result + id;
-		result = prime * result + ((sightingIds == null) ? 0 : sightingIds.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Track other = (Track) obj;
-		if (fused == null) {
-			if (other.fused != null)
-				return false;
-		} else if (!fused.equals(other.fused))
-			return false;
-		if (id != other.id)
-			return false;
-		if (sightingIds == null) {
-			if (other.sightingIds != null)
-				return false;
-		} else if (!sightingIds.equals(other.sightingIds))
-			return false;
-		return true;
-	}
-
-	@Override
 	public String toString() {
 		return "Track [id=" + id + ", fused=" + fused + ", sightingIds=" + sightingIds + "]";
 	}
